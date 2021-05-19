06fa2f111223eb1d7fbd4e341fd9dd6064962ade
Sagar Badu <sagar.badu29@gmail.com>
2021-05-18 21:51:09 -0500

    Third commit

--- Assignment1/src/sumOfArray.java
+++ Assignment1/src/sumOfArray.java
@@ -1,5 +1,22 @@
 
 public class sumOfArray {
+	
+	
+       public static void main(String[] args) {
+		
+	    // An array which holds 5 numbers entered by a user
+		int numbers[] = {10, 11, 12, 13, 14};
+		
+		//Prints the result
+		System.out.println("The sum of these numbers is: " + List(numbers));
+		
+		// An array which holds 5 numbers entered by a user
+		 int nums[] = {1,2,3,4,5};
+		 
+		//Prints the result
+		  System.out.println("The product of these numbers is: " + Array(nums));
+
+		}
 
 	//Finding a sum of an array
 		public static int List(int numbers[]) {
